2633bbaf6c7c6c45fe2c7b2b2c2ba6fcd15546d2
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-04-20 19:41:02 -0400

    Complete assignemnt

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,51 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.limit = limit
+        self.items = items
 
     def isEmpty(self):
-        pass
+        print(self.items)
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            return False
+            # raise Exception('Not today pal')
 
     def pop(self):
-        pass
+        if len(self.items) > 0:
+            return self.items.pop(len(self.items) - 1)
+        else:
+            return None
 
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+
+        targ_index = -1
+
+        for i in range(len(self.items)):
+            if self.items[i] == target:
+                targ_index = i
+                break
+        
+        if targ_index != -1:
+            return len(self.items) - targ_index -1
+        else:
+            return targ_index
